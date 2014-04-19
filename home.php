bed8d60144ed4028abe16a621216a8e1d32524aa
Orest Macsanta <orest.macsanta@gmail.com>
2014-04-19 16:10:07 +0300

    logout system
    Repaired bug with logout from account.

--- home.php
+++ home.php
@@ -1,7 +1,5 @@
 <?php
-	include_once ('auth_check.php');
-	
-	echo 'Привет! '.$userdata['users_login'];
-	
-	include ('logout.php');
+        include_once ('auth_check.php');
+        echo 'Привет! '.$userdata['users_login'];
 ?>
+<form action="logout.php" method="post"><input type='submit' name='exit' value='Выйти'/></form>
--- logout.php
+++ logout.php
@@ -1,9 +1,8 @@
-<form action="" method="post"><input type='submit' name='exit' value='Выйти'/></form>
 <?php
-	if($_POST['exit']) 
-		{
-					setcookie('id', '', time() - 60*60*24*30, '/'); 
-					setcookie('hash', '', time() - 60*60*24*30, '/');
-					header('Location: login.php'); exit();
-		}
+  if($_POST['exit'])
+     {
+      setcookie('id', '', time() - 60*60*24*30, '/');
+      setcookie('hash', '', time() - 60*60*24*30, '/');
+      header('Location: login.php'); exit();
+     }
 ?>
