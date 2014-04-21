36432d17276a60ae8afc8dc561a636a795c0e80f
Orest Macsanta <orest.macsanta@gmail.com>
2014-04-21 11:55:15 +0300

    accounts system

--- home.php
+++ home.php
@@ -1,5 +1,5 @@
 <?php
         include_once ('auth_check.php');
-        echo 'Привет! '.$userdata['users_login'];
+        echo 'Привет! '.$userdata['users_name'];
 ?>
 <form action="logout.php" method="post"><input type='submit' name='exit' value='Выйти'/></form>
--- register.php
+++ register.php
@@ -29,16 +29,22 @@
 					 
 					$login = $_POST['login'];  
 					$password = md5(md5(trim($_POST['password']))); 
+					$name = $_POST['name'];
+					$email = $_POST['email'];
+					$phone = $_POST['phone'];
 					 
-					mysql_query("INSERT INTO users SET users_login='".$login."', users_password='".$password."'"); 
+					mysql_query("INSERT INTO users SET users_login='".$login."', users_password='".$password."', users_name='".$name."',users_email='".$email."', users_phone='".$phone."'"); 
 					header("Location: login.php"); exit(); 
 			}
 	} 
 	?>
 
 		<form method="POST" action="">
-		Логин <input type="text" name="login" id="reg_inp" /><br />
-		Пароль <input type="password" name="password" id="reg_inp" /><br />
+		Логин<br><input type="text" name="login" id="reg_inp" required /><br />
+		Пароль<br><input type="password" name="password" id="reg_inp" required /><br />
+		Ваше им'я<br><input type="text" name="name" id="reg_inp" required /><br />
+		E-mail<br><input type="text" name="email" id="reg_inp" required /><br />
+		Телефон<br><input type="text" name="phone" id="reg_inp" /><br />
 		<input name="submit" type="submit" value="Зарегистрироваться"> 
 		</form>
 		<?php
